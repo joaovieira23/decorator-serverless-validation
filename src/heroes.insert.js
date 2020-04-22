0136bb039803096ba5230f5d021a9cd7a5a00cd5
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-22 03:16:25 -0300

    feat: define data insertion in DynamoDB

--- src/heroes.insert.js
+++ src/heroes.insert.js
@@ -1,19 +1,62 @@
+const uuid = require('uuid')
 class Handler {
-  async main() {
+  constructor({ dynamoDbSvc }) {
+    this.dynamoDbSvc = dynamoDbSvc
+    this.dynamodbTable = process.env.DYNAMODB_TABLE
+  }
+
+  async insertItem(params) {
+    return this.dynamoDbSvc.put(params).promise()
+  }
+
+
+  prepareData(data) {
+    const params = {
+      TableName: this.dynamodbTable,
+      Item: {
+        ...data,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString()
+      }
+    }
+    console.log(params)
+    return params
+  }
+
+  handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+    return response
+  }
+
+  handlerError(data) {
+    return {
+      statusCode: data.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create item!!!'
+    }
+  }
+
+  async main(event) {
     try {
-      return {
-        statusCode: 200,
-        body: 'Hello João Vieira!'
-      }
+      const data = JSON.parse(event.body)
+      const dbParams = this.prepareData(data)
+      await this.insertItem(dbParams)
+      
+      return this.handlerSuccess(dbParams.Item)
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: 'Internal server error'
-      }
+      console.error('Deu ruim***', error.stack)
+      return this.handlerError({ statusCode: 500 })
     }
   }
 }
 
 //factory
-const handler = new Handler();
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+const handler = new Handler({
+  dynamoDbSvc: dynamoDB
+});
 module.exports = handler.main.bind(handler)
